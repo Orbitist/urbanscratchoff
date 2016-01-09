d05c5ab109451c4ece7ca10165606005dbb44fef
Nicholas Gunner <ngunner@Nicholass-MacBook-Pro-2.local>
2016-01-09 15:25:04 -0500

    dropped spinner

--- plugins/orbitist/orbitist.js
+++ plugins/orbitist/orbitist.js
@@ -30,13 +30,6 @@
 	}).addTo(map);
 // END ORBITIST POI
 
-var $ = 'jQuery';
-
-$(document).ready(function() {
-	$(this).find(".spinner").delay(2000).fadeOut(500, function () {
-		$(this).remove();
-	});
-});
 
 window.setInterval(function(){
 	twttr.widgets.load()
